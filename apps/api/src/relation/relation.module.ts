a3b297e7ecbb2c72b554076fd1dcb7a451af71f5
Sebastian Angelone <sebastian.angelone@stark-deutschland.de>
2026-06-09 13:50:36 +0200

    fix: add TypeOrmModule.forFeature to RelationModule

--- apps/api/src/relation/relation.module.ts
+++ apps/api/src/relation/relation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tenant } from '../tenants/tenant.entity';
 import { RelationController } from './relation.controller';
 import { RelationService } from './relation.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tenant])],
   controllers: [RelationController],
   providers: [RelationService],
   exports: [RelationService],
